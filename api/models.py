dfce8b18121253ec2299b7f6cded0e52ff6a04c6
Benjamin D Adams <badams888@hotmail.com>
2020-06-26 18:01:55 -0400

    user,book,note models and started serializers

--- api/models.py
+++ api/models.py
@@ -3,4 +3,29 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+date_joined = models.DateTimeField(auto_now_add=True)
+
+
+
+class Book(models.Model):
+
+    class BookStatusChoice(models.TextChoices):
+        to_read = 'to_read'
+        reading = 'reading'
+        read = 'read'
+        BOOK_STATUS_CHOICES = [
+            (to_read, 'To Read'),
+            (reading, 'Reading'),
+            (read, 'Read'),
+        ]
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="book_users")
+
+    status = model.CharField(max_length=7), choices=BookStatusChoice.choices, default=BookStatusChoice.to_read)
+
+    title = model.textField(max_length=100),
+
+
+class Notes(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True),
+    body = model.CharField(max_length=200),
+    page_num = model.charfield(max_length=10),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import User
+# from api.serializers import
+
 
 # Create your views here.
